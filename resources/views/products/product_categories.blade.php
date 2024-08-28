bd4d2426007384e6b28694bdc26d25dde835508a
NiltonRamosE <niltonencarnacion17@gmail.com>
2024-08-27 23:30:03 -0500

    feat: product view and shop car view

--- resources/views/products/product_categories.blade.php
+++ resources/views/products/product_categories.blade.php
@@ -4,16 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CarpiShop</title>
+    <title>Geanela's Shop</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
-    <link rel="stylesheet" href="./css/main.css">
+    <link rel="icon" href="{{asset('img/test_icon.svg')}}" type="image/x-icon">
+    <link rel="stylesheet" href="{{asset('css/product.css')}}">
 </head>
 <body>
 
     <div class="wrapper">
         <header class="header-mobile">
-            <h1 class="logo">CarpiShop</h1>
+            <h1 class="logo"><a href="{{ route('index') }}">Geanela's Shop</a></h1>
             <button class="open-menu" id="open-menu">
                 <i class="bi bi-list"></i>
             </button>
@@ -23,7 +24,7 @@
                 <i class="bi bi-x"></i>
             </button>
             <header>
-                <h1 class="logo">CarpiShop</h1>
+                <h1 class="logo"><a href="{{ route('index') }}">Geanela's Shop</a></h1>
             </header>
             <nav>
                 <ul class="menu">
@@ -40,14 +41,14 @@
                         <button id="pantalones" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Pantalones</button>
                     </li>
                     <li>
-                        <a class="boton-menu boton-carrito" href="./carrito.html">
+                        <a class="boton-menu boton-carrito" href="{{ route('product.shopcar') }}">
                             <i class="bi bi-cart-fill"></i> Carrito <span id="numerito" class="numerito">0</span>
                         </a>
                     </li>
                 </ul>
             </nav>
             <footer>
-                <p class="texto-footer">© 2022 Carpi Coder</p>
+                <p class="texto-footer">© 2024 Geanela's Shop</p>
             </footer>
         </aside>
         <main>
@@ -59,7 +60,7 @@
     </div>
     
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
-    <script src="./js/main.js"></script>
-    <script src="./js/menu.js"></script>
+    <script src="{{asset('js/main.js')}}"></script>
+    <script src="{{asset('js/menu.js')}}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
+
+Route::get('/products', function () {
+    return view('products/product_categories');
+})->name('product.index');
+
+Route::get('/products/shopCar', function () {
+    return view('products/shop_car');
+})->name('product.shopcar');
