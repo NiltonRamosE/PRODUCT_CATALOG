02ed86122d417a0b9e9bf69a735b04ee9138a837
NiltonRamosE <niltonencarnacion17@gmail.com>
2024-08-29 18:52:30 -0500

    refactor: delete comments and js obsolet

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -16,12 +16,12 @@ class ProductSeeder extends Seeder
             ['TECNO_L_00002', 'MacBook Air M2', 'Laptop ultraligera con procesador M2, ideal para trabajo portátil.', 1299.99, true, 30],
             ['TECNO_AE_00003', 'Cargador Inalámbrico Qi', 'Cargador rápido para dispositivos compatibles con Qi.', 49.99, true, 200],
         
-            ['ELECT_R_00004', 'Refrigerador Samsung', 'Refrigerador con tecnología de ahorro energético.', 799.99, true, 25],
+            ['ELECT_R_00004', 'Refrigerador Samsung', 'Refrigerador con tecnología de ahorro energético.', 799.99, false, 0],
             ['ELECT_L_00005', 'Lavadora LG', 'Lavadora de alta eficiencia con múltiples modos de lavado.', 599.99, true, 15],
             ['ELECT_M_00006', 'Horno Microondas Panasonic', 'Microondas con tecnología Inverter para cocción uniforme.', 199.99, true, 40],
         
             ['MUEBL_S_00007', 'Sofá Seccional', 'Sofá amplio y cómodo, ideal para salas grandes.', 499.99, true, 10],
-            ['MUEBL_C_00008', 'Cama King Size', 'Cama de tamaño king con colchón ortopédico.', 899.99, true, 8],
+            ['MUEBL_C_00008', 'Cama King Size', 'Cama de tamaño king con colchón ortopédico.', 899.99, false, 0],
             ['MUEBL_MDC_00009', 'Mesa de Comedor Moderna', 'Mesa de comedor de estilo moderno con capacidad para 6 personas.', 299.99, true, 12],
         
             ['ROPA_RC_00010', 'Camiseta Casual', 'Camiseta de algodón para uso diario.', 19.99, true, 100],
@@ -29,7 +29,7 @@ class ProductSeeder extends Seeder
             ['ROPA_RD_00012', 'Sudadera Deportiva', 'Sudadera cómoda y ligera para entrenamiento.', 49.99, true, 50],
         
             ['BELLZ_CDLP_00013', 'Crema Hidratante', 'Crema facial para piel seca y sensible.', 24.99, true, 150],
-            ['BELLZ_M_00014', 'Set de Maquillaje Completo', 'Kit completo de maquillaje con sombras, labiales y más.', 79.99, true, 75],
+            ['BELLZ_M_00014', 'Set de Maquillaje Completo', 'Kit completo de maquillaje con sombras, labiales y más.', 79.99, false, 0],
             ['BELLZ_HP_00015', 'Jabón Antibacterial', 'Jabón líquido con propiedades antibacteriales para higiene diaria.', 5.99, true, 300],
         ];
         
